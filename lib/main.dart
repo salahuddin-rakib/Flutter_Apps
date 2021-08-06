2352029a28631c388bf5f865f0d7cb5f1d61a1a1
Rakib <rakib@misfit.tech>
2021-08-07 02:17:28 +0600

    Added image showing functionalities.

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,41 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Home(),
-  ),);
+void main() => runApp(
+      MaterialApp(
+        home: Home(),
+      ),
+    );
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Text('My First App'),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
-      child: Text(
-        'Hello Gazi Salahuddin Rakib.',
-        style: TextStyle(
-          fontSize: 25.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 1.5,
-          color: Colors.grey[600],
-          fontFamily: 'DancingScript',
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {},
-      child: Text('Click'),
-      backgroundColor: Colors.red[600],
-    ),
-  );
+      body: Center(
+        // Displaying an online image in app
+        // child: Image(
+        //   image: NetworkImage('https://images.unsplash.com/photo-1508402476522-c77c2fa4479d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80'),
+        // ),
+        // Shortcut of online image showing
+        // child: Image.network(
+        //     'https://images.unsplash.com/photo-1508402476522-c77c2fa4479d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'),
+
+        // Displaying an assets image in app
+        // child: Image(
+        //   image: AssetImage('assets/stars-3.jpeg'),
+        // ),
+        // Shortcut of assets image showing
+        child: Image.asset('assets/stars-1.jpeg'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Click'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
   }
 }
