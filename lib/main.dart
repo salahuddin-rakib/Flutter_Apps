070ea93312ed3cf4d2cf25dde5d0585fe6d7b189
Rakib <rakib@misfit.tech>
2021-08-07 00:53:40 +0600

    Activated hot reload into project.

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('My First App'),
-        centerTitle: true,
-        backgroundColor: Colors.red[600],
-      ),
-      body: Center(
-        child: Text(
-            'Hello Gazi Salahuddin Rakib.',
-             style: TextStyle(
-               fontSize: 25.0,
-               fontWeight: FontWeight.bold,
-               letterSpacing: 1.5,
-               color: Colors.grey[600],
-               fontFamily: 'DancingScript',
-             ),
+void main() => runApp(MaterialApp(
+      home: Home(),
+  ),);
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('My First App'),
+      centerTitle: true,
+      backgroundColor: Colors.red[600],
+    ),
+    body: Center(
+      child: Text(
+        'Hello Gazi Salahuddin Rakib.',
+        style: TextStyle(
+          fontSize: 25.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 1.5,
+          color: Colors.grey[600],
+          fontFamily: 'DancingScript',
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text('Click'),
-        backgroundColor: Colors.red[600],
-      ),
     ),
-  ));
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {},
+      child: Text('Click'),
+      backgroundColor: Colors.red[600],
+    ),
+  );
+  }
 }
