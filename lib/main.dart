ee10f7122d77d6f2a520e3c8220cc1bfabfdd181
Rakib <rakib@misfit.tech>
2021-08-07 13:42:09 +0600

    Examples of Container and Padding widgets.

--- lib/main.dart
+++ lib/main.dart
@@ -15,57 +15,37 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Center(
-        // Icon adding to the project.
-        // child: Icon(
-        //   Icons.airport_shuttle,
-        //   color: Colors.lightBlue,
-        //   size: 100.0,
-        // ),
 
-        // ***** Example of different types of button *****
+      // Container example:
+      // body: Container(
+      //   // Padding example
+      //
+      //   // It will take 30 pixel padding all around of it's body.
+      //   // padding: EdgeInsets.all(30.0),
+      //
+      //   // It will take 10 pixel Left, 20 pixel Top, 30 pixel Right and 40 pixel bottom padding around it's body.
+      //   // padding: EdgeInsets.fromLTRB(10, 20, 30, 40),
+      //
+      //   // It will take 10 pixel horizontally and 20 pixel vertically padding around it's body.
+      //   padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+      //
+      //   // It will take 10 pixel margin all around of it's body.
+      //   // margin: EdgeInsets.all(10.0),
+      //
+      //   // It will take 10 pixel horizontally and 20 pixel vertically margin around it's body.
+      //   // We can also use all functions here like padding.
+      //   margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+      //
+      //   // If we don't give any child then it will take whole body space.
+      //   color: Colors.grey[400],
+      //   child: Text('Hello'),
+      // ),
 
-        // ElevatedButton example
-        // child: ElevatedButton(
-        //   onPressed: () {},
-        //   child: Text('click me'),
-        // ),
-
-        // TextButton example
-        // child: TextButton(
-        //   onPressed: () {},
-        //   child: Text('click me'),
-        // ),
-
-        // Adding something workable to onPressed action
-        // child: ElevatedButton(
-        //   onPressed: () {
-        //     print('You clicked me.');
-        //   },
-        //   child: Text('click me'),
-        // ),
-
-        // Adding a Icon and Text Button
-        // child: ElevatedButton.icon(
-        //   onPressed: () {},
-        //   icon: Icon(
-        //     Icons.mail,
-        //     color: Colors.amber,
-        //   ),
-        //   label: Text('mail me'),
-        // ),
-
-        // Example of IconButton
-        child: IconButton(
-          onPressed: () {
-            print('You clicked me.');
-          },
-          icon: Icon(
-            Icons.alternate_email,
-            size: 35.0,
-          ),
-          color: Colors.amber,
-        ),
+      // If we only want to use padding into a text without container, we should do that this way.
+      // We can't use margin or color widgets inside Padding widget. To do that we should use Container widget.
+      body: Padding(
+        padding: EdgeInsets.all(30.0),
+        child: Text('Hello'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
